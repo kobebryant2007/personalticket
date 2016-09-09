c048b382eba9604bb1338941e63d1dfa9868d634
bryant <kobebryant2007@163.com>
2016-09-09 15:44:43 +0800

    改变文件

--- web/src/main/java/com/airchina/controller/SpringBootTestController.java
+++ web/src/main/java/com/airchina/controller/SpringBootTestController.java
@@ -1,9 +1,15 @@
 package com.airchina.controller;
 
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @EnableAutoConfiguration
@@ -17,9 +23,11 @@ public class SpringBootTestController {
 
     @RequestMapping("/next/{id}")
     @ResponseBody
-    String next(@PathVariable("id") Long id){
+    Map next(@PathVariable("id") Long id){
         System.out.println(id);
-        return "进行下一步";
+        Map<String,Long> list = new HashMap<String,Long>();
+        list.put("hello",id);
+        return list;
     }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootTestController.class, args);
