658feadddee19b736847504702aef9204870d3e6
bryant <kobebryant2007@163.com>
2016-09-09 11:45:22 +0800

    改变文件

--- web/src/main/java/com/airchina/controller/SpringBootTestController.java
+++ web/src/main/java/com/airchina/controller/SpringBootTestController.java
@@ -5,16 +5,22 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SpringBootTestController {
 
     @RequestMapping("/web")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello  every World!";
     }
 
+    @RequestMapping("/next/{id}")
+    @ResponseBody
+    String next(@PathVariable("id") Long id){
+        System.out.println(id);
+        return "进行下一步";
+    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootTestController.class, args);
     }
